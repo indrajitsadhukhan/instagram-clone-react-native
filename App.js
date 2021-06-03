5830059b1a9f9cbca10791c3d6243a1f84180c3d
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2021-06-03 22:20:28 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+//33:00
+
+import {StatusBar} from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import LandingScreen from './components/auth/Landing'
+
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Landing">
+        <Stack.Screen name = "Landing" component ={LandingScreen} options ={{headerShown:false}}>
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
